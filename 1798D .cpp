0030ffac2a6908120f3d41a1d44014907c5a36c2
Thasin-Sheikh <thasinsheikh1998@gmail.com>
2023-03-28 22:47:23 +0600

    commiting code solved by me in CF OJ

--- 1798D .cpp
+++ 1798D .cpp
@@ -19,7 +19,7 @@ const long N = 3e5 + 10;
 const long maxn=5e5+10;
 const long long  maxval=1e18;
 const long long minval=-1e18;
-typedef tree<ll ,null_type,less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update>os;
+typedef tree<ll,null_type,less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update>os;
 ll aarray[N];
 ll magic[R][R];
 vector<ll>primes;
@@ -37,10 +37,83 @@ map<ll,ll>mp;
 //priority_queue<ll>pq;// from max to min
 void solve()
 {
-    ll i,j,k,m,n,l,r,q,p,ans,sum=0,a,b,c,d,mi=maxval,ma=minval;
+    ll i,j,k,m,n,l,r,q,p,sum=0,a,b,c,d,mi=maxval,ma=minval;
     string str;
     bool yes=true;
-    vector<ll>v;
+    vector<ll>v,po,ne,ans;
+    cin>>n;
+    set<ll>s;
+    for(i=1; i<=n; i++)
+    {
+        cin>>aarray[i];
+        s.insert(aarray[i]);
+        if(aarray[i]<0)
+        {
+            ne.push_back(aarray[i]);
+        }
+        else
+        {
+            po.push_back(aarray[i]);
+        }
+
+    }
+    if(s.size()==1||po.size()==n)
+    {
+        cout<<"NO"<<"\n";
+        return ;
+    }
+    cout<<"YES"<<"\n";
+    sort(ne.rbegin(),ne.rend());
+    sort(po.begin(),po.end());
+    ll psum=0;
+    ll nsum=0;
+    psum+=po.back();
+    ans.push_back(po.back());
+    po.pop_back();
+    while(1)
+    {
+        if(ne.size()==0&&po.size()==0)
+        {
+            break;
+        }
+        if(po.size()&&ne.size()==0)
+        {
+            while(po.size())
+            {
+                ans.push_back(po.back());
+                po.pop_back();
+            }
+            break;
+
+        }
+        if(po.size()==0&&ne.size())
+        {
+            while(ne.size())
+            {
+                ans.push_back(ne.back());
+                ne.pop_back();
+            }
+            break;
+
+        }
+        if(nsum+abs(ne.back())<=psum)
+        {
+            nsum+=abs(ne.back());
+            ans.push_back(ne.back());
+            ne.pop_back();
+        }
+        else
+        {
+            psum+=po.back();
+            ans.push_back(po.back());
+            po.pop_back();
+        }
+    }
+    for(auto it:ans)
+    {
+        cout<<it<<" ";
+    }
+    cout<<"\n";
 }
 int main()
 {
